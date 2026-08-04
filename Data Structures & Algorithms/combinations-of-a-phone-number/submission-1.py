398e3bc881bc2cca71848f843d798cfc55328359
Sherman Liu <shermanliu03@gmail.com>
2026-08-03 23:44:59 -0400

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def __init__(self):
+        self.res = []
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+            
+        digits_map = {
+            "2":"abc", 
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+
+        self.explore(digits, 0, [], digits_map)
+        return self.res 
+
+    def explore(self, digits, digit_i, state:List[str], digits_map):
+        if digit_i >= len(digits):
+            self.res.append("".join(state))
+            return 
+        for c in digits_map[digits[digit_i]]:
+            state.append(c)
+            self.explore(digits, digit_i+1, state, digits_map)
+            state.pop()
+            
+        
+
+
+
+        
